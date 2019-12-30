8ae61191ae5226539c37c3c9309b87b0142e3067
Joakim Hedlund <joakim.hedlund@danielwellington.com>
2019-12-30 14:17:24 +0100

    Obviously we need to start CUPS before sending it commands

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+# Start cups
+/usr/sbin/cupsd
+
+# Install the printer
 lpadmin -p dymo -v $(lpinfo -v | grep -o 'usb://DYMO.*$') -P /usr/share/cups/model/lw450.ppd
 
-/usr/sbin/cupsd && node index.js
+# Start the printer
+cupsenable dymo
+
+# Accept jobs to the printer
+cupsaccept dymo
+
+# Run our node server
+node index.js
